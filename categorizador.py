be0d4c9081f260c7147b9acc84cd139742e0ce48
Raimundo Vieira <raimundoruan61@gmail.com>
2025-07-18 00:59:44 -0300

    Criando a main e o categorizador.
    Criando a main e o categorizador.
    
    Criei a main onde o usuário faz uma pergunta referente a um e-commerce de produtos sustentáveis e um categorizador de produtos.
    
    Utilizando a Gemini-AI

---
+++ categorizador.py
@@ -0,0 +1,40 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHAVE_API_GOOGLE=os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=CHAVE_API_GOOGLE)
+MODELO_ESCOLHIDO='gemini-2.5-flash'
+def categorizar_produto(nome_produto,list_of_possible_categories):
+    prompt_system=f"""
+        Você é um categorizador de produtos.
+        Você deve assumir as categorias presentes na lista abaixo.
+        # Lista de Categorias Válidas
+        {list_of_possible_categories.split(",")}
+        # Formato da Saída
+        Produto: Nome do Produto
+        Categoria: apresente a categoria do produto
+        # Exemplo de Saída
+        Produto: Escova elétrica com recarga solar
+        Categoria: Eletrônicos Verdes
+        """
+    llm=genai.GenerativeModel(
+        model_name=MODELO_ESCOLHIDO,
+        system_instruction=prompt_system
+    )
+    
+    resposta=llm.generate_content(nome_produto)
+    return resposta.text
+
+def main():
+    list_of_possible_categories='Eletrônicos Verdes,Moda Sustentável,Produtos de Limpeza,Alimentos Orgânicos, Produtos de Higienes Sustentáveis'
+    
+    produto=input('Informe o produto que você deseja classificar: ')
+
+    while produto!="":
+        print(f'Resposta:\n{categorizar_produto(produto,list_of_possible_categories)}')
+        produto=input('Informe o produto que você deseja classificar: ')
+if __name__=="__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHAVE_API_GOOGLE=os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=CHAVE_API_GOOGLE)
+MODELO_ESCOLHIDO='gemini-2.5-flash'
+
+prompt_system='Liste apenas os nomes dos produtos, e ofereça uma breve descrição.'
+
+configure_model={
+    "temperature" : 2.0,
+    "top_p" : 0.9,
+    "top_k" : 64,
+    "max_output_tokens" : 8192,
+    "response_mime_type" : "text/plain"
+}
+
+llm=genai.GenerativeModel(
+    model_name=MODELO_ESCOLHIDO,
+    system_instruction=prompt_system,
+    generation_config=configure_model
+)
+pergunta='Liste três produtos de moda sustentável para ir ao shopping.'
+resposta=llm.generate_content(pergunta)
+print(f'A resposta gerada para a pergunta é:{resposta.text}')
